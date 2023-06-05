8242fd087a9648b351cb28fb334a9c296ac2ab02
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-06-04 21:32:18 -0600

    acha

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const [text, setText] = useState("Enter your text here");
+
+  const [intialWord, wordUpdated] = useState(16);
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
@@ -9,22 +11,39 @@ export default function TextForm(props) {
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
-  return (
-    <div>
-      <h1>{props.heading}</h1>
 
-      <div className="form-group">
-        <textarea
-          onChange={handleOnChange}
-          className="form-control"
-          value={text}
-          id="my-Box"
-          rows="8"
-        ></textarea>
+  const WordCount = (text) => {
+    wordUpdated(text.split(" ").length);
+  };
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+
+        <div className="form-group">
+          <textarea
+            onChange={handleOnChange}
+            className="form-control"
+            value={text}
+            id="my-Box"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary my-3 mx-3" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button
+          className="btn btn-primary my-3"
+          onClick={() => WordCount(text)}
+        >
+          Number of Words
+        </button>
       </div>
-      <button className="btn btn-primary my-3" onClick={handleUpClick}>
-        Convert to uppercase
-      </button>
-    </div>
+      <div className="container mx-3">
+        <h3>Your text Summary</h3>
+        <p>Number of Words {intialWord} </p>
+        <p>Number of characters {text.length}</p>
+      </div>
+    </>
   );
 }
