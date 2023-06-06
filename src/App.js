e2cc35eb6889996f66fba3832a4fb9fd88de6ab8
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-06-06 04:31:42 -0600

    okay

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import TextForm from "./components/TextForm";
 import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
+
 function App() {
   const [mode, setMode] = useState("light"); //whether dark mood unabled or not
   const [alert, setAlert] = useState(null);
@@ -29,18 +35,31 @@ function App() {
   };
   return (
     <>
-      <Navbar
-        title="TextUtils"
-        aboutText="About"
-        mode={mode}
-        toggle={toggleMode}
-      />
-      <Alert alert={alert} />
-      <div className="container my-5">
-        <TextForm heading="Text to test" mode={mode} Showalert={showAlert} />
-      </div>
-      <footer id="customFooter">Coded by Qazi Saboor</footer>
-      {/* <About /> */}
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About"
+          mode={mode}
+          toggle={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm
+                heading="Text to test"
+                mode={mode}
+                Showalert={showAlert}
+              />
+            </Route>
+          </Switch>
+
+          {/* <footer id="customFooter">Coded by Qazi Saboor</footer> */}
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -71,99 +71,107 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div
-        className="container"
-        style={{
-          backgroundColor: props.mode === "dark" ? "#1e0b69" : "white",
-          color: props.mode === "dark" ? "white" : "black",
-        }}
-      >
-        <h1>{props.heading}</h1>
-
-        <div className="form-group">
-          <textarea
-            onChange={handleOnChange}
-            className="form-control"
-            value={text}
-            id="my-Box"
-            rows="8"
-            style={{
-              backgroundColor: props.mode === "dark" ? "#1e0b69" : "white",
-              color: props.mode === "dark" ? "white" : "black",
-            }}
-          ></textarea>
-        </div>
-        <button
-          className="btn btn-primary "
-          onClick={handleUpClick}
-          style={Style}
-        >
-          Convert to upper Case
-        </button>
-        <button
-          className="btn btn-primary "
-          onClick={handleloClick}
-          style={Style}
-        >
-          Convert to lower Case
-        </button>
-        <button
-          style={Style}
-          className="btn btn-primary "
-          onClick={() => {
-            captilazeEachWord(text);
+      <div className="container">
+        <div
+          className="container"
+          style={{
+            backgroundColor: props.mode === "dark" ? "#1e0b69" : "white",
+            color: props.mode === "dark" ? "white" : "black",
           }}
         >
-          Captilize each word.
-        </button>
+          <h1>{props.heading}</h1>
 
-        <button className="btn btn-primary " onClick={speak} style={Style}>
-          Read Text
-        </button>
+          <div className="form-group">
+            <textarea
+              onChange={handleOnChange}
+              className="form-control"
+              value={text}
+              id="my-Box"
+              rows="8"
+              style={{
+                backgroundColor: props.mode === "dark" ? "#1e0b69" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
+            ></textarea>
+          </div>
+          <button
+            className="btn btn-primary "
+            onClick={handleUpClick}
+            style={Style}
+          >
+            Convert to upper Case
+          </button>
+          <button
+            className="btn btn-primary "
+            onClick={handleloClick}
+            style={Style}
+          >
+            Convert to lower Case
+          </button>
+          <button
+            style={Style}
+            className="btn btn-primary "
+            onClick={() => {
+              captilazeEachWord(text);
+            }}
+          >
+            Captilize each word.
+          </button>
 
-        <button className="btn btn-primary " onClick={handleCopy} style={Style}>
-          Copy text
-        </button>
+          <button className="btn btn-primary " onClick={speak} style={Style}>
+            Read Text
+          </button>
 
-        <button
-          className="btn btn-primary "
-          onClick={handleExtraSpaces}
-          style={Style}
+          <button
+            className="btn btn-primary "
+            onClick={handleCopy}
+            style={Style}
+          >
+            Copy text
+          </button>
+
+          <button
+            className="btn btn-primary "
+            onClick={handleExtraSpaces}
+            style={Style}
+          >
+            remove extra spaces
+          </button>
+
+          <button
+            className="btn btn-danger my-3 mx-3"
+            onClick={() => clearText()}
+            style={clearStyle}
+          >
+            Clear Text
+          </button>
+        </div>
+        <div
+          className="container my-5"
+          style={{
+            backgroundColor: props.mode === "dark" ? "#1e0b69" : "white",
+            color: props.mode === "dark" ? "white" : "black",
+          }}
         >
-          remove extra spaces
-        </button>
+          <h3>Your text Summary</h3>
+          <p>
+            Number of Words:
+            {text.split(" ").length - 1}
+          </p>
+          <p>
+            Number of characters:
+            {text.length}
+          </p>
 
-        <button
-          className="btn btn-danger my-3 mx-3"
-          onClick={() => clearText()}
-          style={clearStyle}
-        >
-          Clear Text
-        </button>
-      </div>
-      <div
-        className="container my-5"
-        style={{
-          backgroundColor: props.mode === "dark" ? "#1e0b69" : "white",
-          color: props.mode === "dark" ? "white" : "black",
-        }}
-      >
-        <h3>Your text Summary</h3>
-        <p>
-          Number of Words:
-          {text.split(" ").length - 1}
-        </p>
-        <p>
-          Number of characters:
-          {text.length}
-        </p>
-
-        <p>
-          Minutes to read:
-          {(text.split(" ").length - 1) * 0.008}
-        </p>
-        <h4>Preview</h4>
-        <p>{text.length > 0 ? text : "Text entered will be previewed here."}</p>
+          <p>
+            Minutes to read:
+            {(text.split(" ").length - 1) * 0.008}
+          </p>
+          <h4>Preview</h4>
+          <p>
+            {text.length > 0 ? text : "Text entered will be previewed here."}
+          </p>
+        </div>
       </div>
     </>
   );
